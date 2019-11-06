5407fba56d96ed546c0a764e2353671edabb7435
kk <k.klapczynski@gmail.com>
2019-11-06 11:01:22 +0100

    Up to Class 29

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '.app-servers',
+  // selector: '[app-servers]',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServers: boolean = false;
+  serverCreationStatus: string = 'NO server created.';
+
+  constructor() { 
+    // constructor method is run when component is created by angular
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 2000);
+    
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server WAS created.';
+  }
+}
